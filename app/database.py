ddea11bc68ea39f040106cec95361ca8b4be1f1f
Makara Nuol <nuolmakara@gmail.com>
2025-07-19 15:06:17 +0700

    add postrgres database connection

--- app/database.py
+++ app/database.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///fastapi.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# SQLALCHEMY_DATABASE_URL = "sqlite:///fastapi.db"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
 )
+# postgresql+psycopg2://user:passsowrd@host:5432/db_name
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
