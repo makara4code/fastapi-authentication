f08c6f8ea6feb744e1dccb0471784365c64f4c9c
Makara Nuol <nuolmakara@gmail.com>
2025-07-26 15:27:18 +0700

    add compose file for db

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///fastapi.db"
 SQLALCHEMY_DATABASE_URL = (
-    "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
+    "postgresql+psycopg2://myuser:mypassword@127.0.0.1:5432/mydatabase"
 )
 # postgresql+psycopg2://user:passsowrd@host:5432/db_name
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
