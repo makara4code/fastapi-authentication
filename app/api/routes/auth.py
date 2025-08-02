3c247254feaedcabec857b1d1dabbae65a791362
Makara Nuol <nuolmakara@gmail.com>
2025-08-02 11:53:02 +0700

    add rate limiter and docker file

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,37 +1,46 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Request
 from app.api.dependencies import SessionDep
-from pydantic import BaseModel
 from app.models import User
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
 from datetime import timedelta
-from app.services.auth import (bcrypt_context, authenticate, create_access_token)
+from app.services.auth import bcrypt_context, authenticate, create_access_token
 from app.schemas import Token, CreateUserRequest
+from app.rate_limiter import limiter
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 # FormDep = Annotated[OAuth2PasswordRequestForm, Depends()]
 # FormDep = Annotated[OAuth2PasswordRequestForm, Depends(OAuth2PasswordRequestForm)]
-    
+
+
 # /auth/login
 @router.post("/token", response_model=Token)
-async def login(db: SessionDep, form: Annotated[OAuth2PasswordRequestForm, Depends()]):
+@limiter.limit("1/minute")
+async def login(
+    request: Request,
+    db: SessionDep,
+    form: Annotated[OAuth2PasswordRequestForm, Depends()],
+):
     user = authenticate(db, form.username, form.password)
-    
+
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username or password")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid username or password",
+        )
 
     token = create_access_token(user.username, user.id, timedelta(minutes=15))
-    
-    return {
-        "access_token": token,
-        "token_type": "bearer"
-    }
+
+    return {"access_token": token, "token_type": "bearer"}
 
 
 # /auth/register
 @router.post("/register", status_code=201)
-async def register(db: SessionDep, create_user_request: CreateUserRequest):
+@limiter.limit("1/minute")
+async def register(
+    request: Request, db: SessionDep, create_user_request: CreateUserRequest
+):
     # uv add "passlib[bcrypt]"
     # uv add python-jose[cryptography]
     # uv add bcrypt==4.0.1
